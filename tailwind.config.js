62153acf761355a75dbaab79c782bee465517740
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-10-18 19:48:21 -0300

    fix:  fix purge content array

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
   },
   purge: {
     enabled: true,
-    content: ['./pages/**/*{.js,.jsx,.ts,.tsx}']
+    content: [
+      './pages/**/*.{js,jsx,ts,tsx}',
+      './components/**/*.{js,jsx,ts,tsx}'
+    ]
   },
   theme: {
     extend: {}
