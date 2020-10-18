a4fb05a5cb71ff22cb506f42d86280669ed1a74f
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-10-17 22:21:28 -0300

    fix: layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,7 +14,6 @@ const Layout: React.FunctionComponent<LayoutProps> = ({
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        {/* <link rel="icon" href="https://rocketseat.com.br/favicon.ico" /> */}
       </Head>
       <div>{children}</div>
     </>
