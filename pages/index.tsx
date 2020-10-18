ec3e1df366695e126a91a146ef45a1b218a3ccb3
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-10-18 19:43:26 -0300

    update: new home with some tailwind styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,18 @@ import Layout from '../components/Layout';
 const Home: NextPage = () => {
   return (
     <Layout title="Home | Adoptar">
-      <h1>Initial Structure</h1>
+      <section className="flex flex-col items-center w-full">
+        <h1 className="text-4xl leading-tight tracking-wide">
+          Starter pack that includes:
+        </h1>
+        <ul className="flex flex-col list-disc">
+          <li>Next.js</li>
+          <li>TypeScript</li>
+          <li>Tailwind</li>
+          <li>Jest</li>
+          <li>React Testing Library</li>
+        </ul>
+      </section>
     </Layout>
   );
 };
