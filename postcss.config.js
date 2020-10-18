ed28602fc56c59876ebd713dd899e3b213910500
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-10-18 19:43:52 -0300

    update: new config to work on production

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,28 @@
 module.exports = {
-  plugins: ['tailwindcss']
+  plugins: [
+    'tailwindcss',
+    process.env.NODE_ENV === 'production'
+      ? [
+          'postcss-flexbugs-fixes',
+          [
+            'postcss-preset-env',
+            [
+              '@fullhuman/postcss-purgecss',
+              {
+                autoprefixer: {
+                  flexbox: 'no-2009'
+                },
+                stage: 3,
+                features: {
+                  'custom-properties': false
+                },
+                defaultExtractor: content =>
+                  content.match(/[\w-/:]+(?<!:)/g) || []
+              }
+            ]
+          ]
+        ]
+      : undefined,
+    'autoprefixer'
+  ]
 };
